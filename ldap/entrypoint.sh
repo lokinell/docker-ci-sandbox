e798b042c8d61907a4c22e36ee5f6a66ad5678e0
Kirill Evstigneev <k.evstigneev@gmail.com>
2015-04-25 02:38:17 +0300

    LDAP: Migrate to CentOS base image

--- ldap/entrypoint.sh
+++ ldap/entrypoint.sh
@@ -4,53 +4,103 @@
 # It initializes the LDAP DB (1st time) and run CMD process.
 #
 
-[ ! -f /run/lock/initialized ] || exec "$@"
+[ ! -f /var/lock/initialized ] || exec "$@"
 
 
 # The following steps are for initial bootstrapping only
 
 # LDAP administrator
-ldap_admin_dn="cn=admin,dc=asf,dc=griddynamics,dc=com"
+admin_user="admin"
 
 # Expected environment variables
 # to be passed from Docker command line or fig.yml:
 #
 echo "Test environment configuration..."
+[ "$domain" ]
+[ "$organization" ]
+[ "$admin_user" ]
 [ "$ldap_admin_password" ]
 [ "$system_admin_password" ]
 [ "$jenkins_bot_password" ]
 echo "Succeeded"
 
+ldap_domain_name=$(echo "$domain" | cut -f1 -d.)
+ldap_domain_dn=$(echo ".${domain}" | sed 's/\./,dc=/g;s/^,//')
+ldap_admin_dn="cn=${admin_user},${ldap_domain_dn}"
+
 
 # Start LDAP daemon listening for local requests only
 # then feed it with LDAP data.
 #
-slapd -u openldap -h "ldapi:///" -F /etc/ldap/slapd.d
+slapd -u ldap -h "ldapi:///" -F /etc/openldap/slapd.d
 
-# Set password LDAP administrator
+# Set password for LDAP administrator
 #
 ldapmodify -Q -Y EXTERNAL -H ldapi:/// <<_ADMIN_PASSWORD
-dn: olcDatabase={1}hdb,cn=config
+dn: olcDatabase={0}config,cn=config
 changetype: modify
 replace: olcRootPW
 olcRootPW: $(slappasswd -s "$ldap_admin_password")
 -
 _ADMIN_PASSWORD
 
+# Set up the domain
+#
+ldapmodify -Q -Y EXTERNAL -H ldapi:/// <<_LDAP_DOMAIN
+dn: olcDatabase={1}monitor,cn=config
+changetype: modify
+replace: olcAccess
+olcAccess: {0}to *
+  by dn.base="gidNumber=0+uidNumber=0,cn=peercred,cn=external,cn=auth" read
+  by dn.base="${ldap_admin_dn}" read
+  by * none
+
+dn: olcDatabase={2}bdb,cn=config
+changetype: modify
+replace: olcSuffix
+olcSuffix: ${ldap_domain_dn}
+-
+replace: olcRootDN
+olcRootDN: ${ldap_admin_dn}
+-
+add: olcRootPW
+olcRootPW: $(slappasswd -s "$ldap_admin_password")
+-
+add: olcAccess
+olcAccess: {0}to attrs=userPassword,shadowLastChange
+  by dn="${ldap_admin_dn}" write
+  by anonymous auth
+  by self write
+  by * none
+olcAccess: {1}to dn.base=""
+  by * read
+olcAccess: {2}to *
+  by dn="${ldap_admin_dn}" write
+  by * read
+-
+_LDAP_DOMAIN
+
 # Populate LDAP DB with basic entries.
 #
 ldapadd -H ldapi:/// -x -D "$ldap_admin_dn" -w "$ldap_admin_password" <<_ENTITIES
-dn: ou=people,dc=asf,dc=griddynamics,dc=com
+dn: ${ldap_domain_dn}
+objectClass: top
+objectClass: dcObject
+objectclass: organization
+o: ${organization}
+dc: ${ldap_domain_name}
+
+dn: ou=people,${ldap_domain_dn}
 objectclass: organizationalUnit
 ou: people
 
-dn: ou=groups,dc=asf,dc=griddynamics,dc=com
+dn: ou=groups,${ldap_domain_dn}
 objectclass: organizationalUnit
 ou: groups
 
 # System accounts
 
-dn: uid=admin,ou=people,dc=asf,dc=griddynamics,dc=com
+dn: uid=admin,ou=people,${ldap_domain_dn}
 objectclass: inetOrgPerson
 cn: Administrator
 sn: Administrator
@@ -60,7 +110,7 @@ userpassword: $(slappasswd -s "$system_admin_password")
 
 # TODO: Not sure if Jenkins user has to be an inetOrgPerson in ou=people,
 # or not just simpleSecurityObject elsewhere to distinguish from real people.
-dn: uid=jenkins-bot,ou=people,dc=asf,dc=griddynamics,dc=com
+dn: uid=jenkins-bot,ou=people,${ldap_domain_dn}
 objectclass: inetOrgPerson
 cn: Jenkins CI
 sn: Jenkins CI
@@ -69,20 +119,20 @@ userpassword: $(slappasswd -s "$jenkins_bot_password")
 
 # System groups
 
-dn: cn=admins,ou=groups,dc=asf,dc=griddynamics,dc=com
+dn: cn=admins,ou=groups,${ldap_domain_dn}
 objectclass: groupOfNames
 cn: admins
-member: uid=admin,ou=people,dc=asf,dc=griddynamics,dc=com
+member: uid=admin,ou=people,${ldap_domain_dn}
 
-dn: cn=users,ou=groups,dc=asf,dc=griddynamics,dc=com
+dn: cn=users,ou=groups,${ldap_domain_dn}
 objectclass: groupOfNames
 cn: users
-member: uid=admin,ou=people,dc=asf,dc=griddynamics,dc=com
+member: uid=admin,ou=people,${ldap_domain_dn}
 
-dn: cn=robots,ou=groups,dc=asf,dc=griddynamics,dc=com
+dn: cn=robots,ou=groups,${ldap_domain_dn}
 objectclass: groupOfNames
 cn: robots
-member: uid=jenkins-bot,ou=people,dc=asf,dc=griddynamics,dc=com
+member: uid=jenkins-bot,ou=people,${ldap_domain_dn}
 _ENTITIES
 
 # Terminate temporary LDAP daemon, wait a bit to let it exit gracefully
@@ -90,6 +140,6 @@ killall -w slapd
 
 
 # Proceed with CMD
-touch /run/lock/initialized
+touch /var/lock/initialized
 exec "$@"
 
