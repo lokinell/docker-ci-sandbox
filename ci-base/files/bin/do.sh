4b242baae3d5982a6544a8ef72e439c3d8543e2b
Kirill Evstigneev <k.evstigneev@gmail.com>
2015-08-10 01:41:58 +0300

    Entrypoint in ci-base supports volume initialization
    Entrypoint script in ci-base image look to etc/init/volume.d/ directories
    for shell scripts and run them when the container volumes are newly
    provisioned.

--- ci-base/files/bin/do.sh
+++ ci-base/files/bin/do.sh
@@ -7,7 +7,14 @@
 # "-u" Docker CLI option. It may or may not be "root".
 #
 
-set -e
+set -e ${DEBUG:+-x}
+
+run_sh() {
+    for script in "$@"; do
+        [ -f "$script" ] || continue
+        $SHELL "$script"
+    done
+}
 
 on_failure() {
     echo "Initialization failure${script:+: $script exit status $?}" >&2
@@ -15,25 +22,37 @@ on_failure() {
 trap on_failure EXIT
 
 
-# Run it when the container is created (on `docker run`).
+# Initialize container filesystem when it is created (on `docker run`).
 # It handles service creation or upgrade.
 #
-# It scans all /opt/<provider>/<package>/ directories, then looks to
-# etc/init/container.d/ and run all *.sh scripts there using Bash.
+# It scans /opt/<package>/ and /opt/<provider>/<package>/ directories, then
+# looks to etc/init/container.d/ and run all *.sh scripts there using Bash.
 #
 # /var/run/ is local to the container, so it is persisted with the container.
 #
 if [ ! -f /var/run/entrypoint/initialized ]; then
 
     echo "Container initialization..."
-    for script in /opt/*/etc/init/container.d/*.sh \
-                  /opt/*/*/etc/init/container.d/*.sh; do
-        [ -f "$script" ] || continue
-        $SHELL "$script"
-    done
+    run_sh /opt/*/etc/init/container.d/*.sh \
+            /opt/*/*/etc/init/container.d/*.sh
     echo "Container initialization complete"
 
     touch /var/run/entrypoint/initialized
+fi
+
+# Initialize container data on Docker volumes.
+# It handles initial service creation.
+#
+# It works like container initialization but looks to etc/init/volume.d/.
+#
+if [ ! -f /var/opt/entrypoint/initialized ]; then
+
+    echo "Volumes initialization..."
+    run_sh /opt/*/etc/init/volume.d/*.sh \
+            /opt/*/*/etc/init/volume.d/*.sh
+    echo "Volumes initialization complete"
+
+    touch /var/opt/entrypoint/initialized
 fi
 
 
